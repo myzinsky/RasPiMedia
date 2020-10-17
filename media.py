72b171781ff04a95e0f394095834924dbf930886
Matthias Jung <matthias@jung.ms>
2020-10-17 17:07:19 +0200

    Added video support

--- media.py
+++ media.py
@@ -6,6 +6,7 @@ import Tkinter as tk
 from array import *
 from PIL import Image, ImageTk
 from pygame import mixer
+import subprocess
 
 GPIO.setmode(GPIO.BCM)
 
@@ -13,6 +14,7 @@ GPIO.setmode(GPIO.BCM)
 STICK  = "/media/pi/INTENSO/"
 BILDER = STICK+"bilder/"
 MUSIK  = STICK+"musik/"
+VIDEOS = STICK+"videos/"
 
 # LEDs konfigurieren:
 LED1 = 21 # Rot
@@ -28,21 +30,27 @@ for i in range(0,10):
     GPIO.setup(T[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(T[i], GPIO.BOTH, callback=lambda _, x=i: OnButtonPress(x+1), bouncetime=300)
 
-# Schalter Konfigurieren:
-S1 = 12
-GPIO.setup(S1, GPIO.IN)
-GPIO.add_event_detect(S1, GPIO.BOTH, callback=lambda x: toggleLED(), bouncetime=50)
-
-# Pause und Stop:
-T1 = 16 # Pause
-T2 = 13 # Stopp
+# Pause, Stop, Auswahl1, Auswahl2 :
+T1 = 12 # Auswahl 1
+T2 = 13 # Auswahl 2
+T3 = 16 # Pause
+T4 = 19 # Stopp 
 GPIO.setup(T1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(T2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(T1, GPIO.BOTH, callback=lambda x: OnPausePress(), bouncetime=300)
-GPIO.add_event_detect(T2, GPIO.BOTH, callback=lambda x: OnStoppPress(), bouncetime=300)
+GPIO.setup(T3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(T4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(T1, GPIO.BOTH, callback=lambda x: toggleLED1(),   bouncetime=300)
+GPIO.add_event_detect(T2, GPIO.BOTH, callback=lambda x: toggleLED2(),   bouncetime=300)
+GPIO.add_event_detect(T3, GPIO.BOTH, callback=lambda x: OnPausePress(), bouncetime=300)
+GPIO.add_event_detect(T4, GPIO.BOTH, callback=lambda x: OnStoppPress(), bouncetime=300)
 
-# Pausenzustand:
+# Zustandsvariablen:
 pause = False
+state1 = False
+state2 = False
+
+# Videoplayer handle:
+omxc = None
 
 def loadAndResizeImage(f):
     global window
@@ -55,28 +63,36 @@ def loadAndResizeImage(f):
 
 def OnPausePress():
     global pause
+    global omxc
     if pause == False:
         pause = True
         mixer.music.pause()
+        if omxc.poll() == None:
+            omxc.stdin.write('p')
     else:
         pause = False
         mixer.music.unpause()
+        if omxc.poll() == None:
+            omxc.stdin.write('p')
 
-def toggleLED():
-        GPIO.output(LED1, GPIO.input(S1));
+def toggleLED1():
+    global state1
+    state1 = not state1
+    GPIO.output(LED1, state1);
+
+def toggleLED2():
+    global state2
+    state2 = not state2
+    GPIO.output(LED2, state2);
 
 def OnStoppPress():
     mixer.music.stop()
+    os.system("killall omxplayer.bin")
 
 def OnButtonPress(button): 
-    if GPIO.input(S1) == True:
-        f = MUSIK+str(button)+".mp3"
-        print(f)
-
-        if os.path.exists(f):
-           mixer.music.load(f)
-           mixer.music.play() 
-    else:	
+    global state1
+    global omxc
+    if state1 == False and state2 == False:	
         global panel
         global img
         f = BILDER+str(button)+".jpg"
@@ -85,33 +101,44 @@ def OnButtonPress(button):
             img = loadAndResizeImage(f)
         else:
             img = loadAndResizeImage(BILDER+"0.jpg")
-
         panel['image'] = img
+        os.system("killall omxplayer.bin")
+    elif state1 == True and state2 == False:
+        f = MUSIK+str(button)+".mp3"
+        print(f)
+
+        if os.path.exists(f):
+           os.system("killall omxplayer.bin")
+           mixer.music.load(f)
+           mixer.music.play() 
+    elif state1 == False and state2 == True:
+        # TODO
+        print(" ")
+    elif state1 == True and state2 == True:
+        f = VIDEOS+str(button)+".mp4"
+        print(f)
+        if os.path.exists(f):
+            os.system("killall omxplayer.bin")
+            mixer.music.stop() 
+            omxc = subprocess.Popen(['omxplayer', '-b', f], stdin=subprocess.PIPE)
 
 def key(e):
     if e.char == "q":
         exit()
 
-def checkStick():
-    if os.path.exists(STICK):
-        GPIO.output(LED2, True);
-        return True
-    else:
-        GPIO.output(LED2, False);
-        return False
-
 # Warte bis der STICK eingesteckt ist:
 start = False
 while start == False:
     # Ueberpruefe ob STICK eingesteckt ist:
-    if checkStick() == True:
+    if os.path.exists(STICK) == True:
         start = True
 
 # MP3 PLayer Initialisieren:
 mixer.init()
 
-# LED nach Schalterstellung Konfigurieren:
-GPIO.output(LED1, GPIO.input(S1));
+# LEDs ausschalten:
+GPIO.output(LED1, False);
+GPIO.output(LED2, False);
 
 # Fenster Anlegen:
 window = tk.Tk()
